1feffde13c0ebeac4e734e89aac86a141bd8d13c
InfernoOnVR <infernoonvr@gmail.com>
2024-09-01 23:00:43 -0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx;
+using Utilla;
+
+namespace GiveMeBrawl
+{
+    [ModdedGamemode("AMBUSH","AMBUSH", Utilla.Models.BaseGamemode.Ambush)]
+    [BepInDependency("org.legoandmars.gorillatag.utilla")]
+    [BepInPlugin("GiveMeAmbush", "Give Me Ambush", "1.0.0")]
+    public class Plugin : BaseUnityPlugin{}
+}
